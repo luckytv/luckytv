f43bf3b71d8059ef52a0884005370937abb3aefd
LuckyTV <36712706+luckytv@users.noreply.github.com>
2018-04-14 00:52:53 -0500

    added video addon

---
+++ _repo/plugin.video.luckytvlivetv/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLmx1Y2t5dHZsaXZldHY=')
+
+name = b.b64decode('W0NPTE9SIHJlZF1MdWNreVR2IExpdmVUdlsvQ09MT1Jd')
+
+#host = b.b64decode('aHR0cDovLzE0NC4yMTcuNzguNzg=')
+host = b.b64decode('aHR0cDovL2xpdmUudGhlY2FibGVndXl0di5jb20=')
+port = b.b64decode('MjU0NjE=')
