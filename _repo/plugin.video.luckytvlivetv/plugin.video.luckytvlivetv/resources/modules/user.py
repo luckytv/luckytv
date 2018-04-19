f99af23660414f8f53169b8829acc48714a1a547
LuckyTV <36712706+luckytv@users.noreply.github.com>
2018-04-18 19:09:58 -0500

    changed live tv host

--- _repo/plugin.video.luckytvlivetv/plugin.video.luckytvlivetv/resources/modules/user.py
+++ _repo/plugin.video.luckytvlivetv/plugin.video.luckytvlivetv/resources/modules/user.py
@@ -4,6 +4,6 @@ id   = b.b64decode('cGx1Z2luLnZpZGVvLmx1Y2t5dHZsaXZldHY=')
 
 name = b.b64decode('W0NPTE9SIHJlZF1MdWNreVR2IExpdmVUdlsvQ09MT1Jd')
 
-#host = b.b64decode('aHR0cDovLzE0NC4yMTcuNzguNzg=')
-host = b.b64decode('aHR0cDovL2xpdmUudGhlY2FibGVndXl0di5jb20=')
+#host = b.b64decode('aHR0cDovLzE5NS4xODEuMTY5Ljg1')
+host = b.b64decode('aHR0cDovL2xpdmUubHVja3l0di5vcmc=')
 port = b.b64decode('MjU0NjE=')
